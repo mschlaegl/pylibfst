bc2895cd0c333e33f0668d1e0be21db292ec7d9c
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2022-07-25 13:17:27 +0200

    cleanup: explicity export functions/submodules in __init__
    fixes linting
     * F403 'from .helpers import *' used; unable to detect undefined names
     * F401 '.helpers.*' imported but unused

--- pylibfst/__init__.py
+++ pylibfst/__init__.py
@@ -3,4 +3,19 @@
 # SPDX-License-Identifier: BSD 3-clause "New" or "Revised" License
 #
 
-from .helpers import *
+from _libfstapi import ffi, lib
+from .helpers import string
+from .helpers import get_scopes_signals
+from .helpers import get_signal_name_by_handle
+from .helpers import fstReaderIterBlocks
+from .helpers import fstReaderIterBlocks2
+
+__all__ = (
+    "lib",
+    "ffi",
+    "string",
+    "get_scopes_signals",
+    "get_signal_name_by_handle",
+    "fstReaderIterBlocks",
+    "fstReaderIterBlocks2",
+)
